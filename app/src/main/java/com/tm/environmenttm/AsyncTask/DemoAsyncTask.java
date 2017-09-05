077beb16ad91c73642509d1bdc1599d436f05154
Tai Mai <taimai0604@gmail.com>
2017-09-05 17:31:38 +0700

    version 0.0.4

---
+++ app/src/main/java/com/tm/environmenttm/AsyncTask/DemoAsyncTask.java
@@ -0,0 +1,8 @@
+package com.tm.environmenttm.AsyncTask;
+
+/**
+ * Created by taima on 09/05/2017.
+ */
+
+public class DemoAsyncTask {
+}
--- app/src/main/java/com/tm/environmenttm/config/ConfigActivity.java
+++ app/src/main/java/com/tm/environmenttm/config/ConfigActivity.java
@@ -1,33 +1,8 @@
 package com.tm.environmenttm.config;
 
-import android.app.Application;
+/**
+ * Created by taima on 09/04/2017.
+ */
 
-import com.github.mikephil.charting.utils.Utils;
-import com.tm.environmenttm.constant.ConstantURL;
-import com.tm.environmenttm.controller.IRESTfull;
-import com.tm.environmenttm.controller.RetrofitClient;
-import com.tm.environmenttm.model.RealmTM;
-import com.tm.environmenttm.model.Type;
-
-import java.util.List;
-
-import io.realm.Realm;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-
-
-public class ConfigActivity  extends Application {
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        //init realm
-        Realm.init(this);
-
-        // initialize the utilities
-        Utils.init(this);
-
-    }
-
-
+public class ConfigActivity {
 }
